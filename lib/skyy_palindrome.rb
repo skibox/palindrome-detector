324dc38cac58e819bee26cca3e43312f05afa315
skyy <m.skibicki3007@gmail.com>
2019-01-11 13:17:12 +0100

    Initialize repo

---
+++ lib/skyy_palindrome.rb
@@ -0,0 +1,6 @@
+require "skyy_palindrome/version"
+
+module SkyyPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/skyy_palindrome/version.rb
@@ -0,0 +1,3 @@
+module SkyyPalindrome
+  VERSION = "0.1.0"
+end
